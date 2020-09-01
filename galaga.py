9398d9bd137df702be3947a8a89ed7095558fc12
smaecrof <smaecrof@iu.edu>
2020-09-01 08:52:46 -0500

    Window ERROR: Window loading error
    Last night I could not figure out why I could not get a basic window to
    display with pygame. I used homebrew to install the necessary packages
    but still even with the most basic window/screen display tests, I could
    not get the issue to resolve. I then went on to read that there have
    been many issues with pygame/python3.6/macOS to which I went ahead and
    reinstalled python3 and pygame and used a different install process and
    everything works as expected. I now have some of the basic features of
    this galaga remake created such as a settings file to add future
    application changes and also a ship class that loads in a bitmap image
    and displays the content on its screen.
    
    09/01/2020

--- galaga.py
+++ galaga.py
@@ -3,15 +3,21 @@
 
 import sys 
 import pygame
+from settings import Settings
+from ship import Ship
+
 
 def run_game():
-    # Initialize game and create a screen object. 
+    # Initialize pygame, settings, and screen object  
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+            (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Galaga")
 
-    # Set background color
-    bg_color = (230, 230, 230)
+
+    # Make Ship Object 
+    ship = Ship(screen)
 
     # Start the main loop for the game
     while True:
@@ -22,10 +28,12 @@ def run_game():
                 sys.exit()
 
         # Redraw the screen during each pass through the loop
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
+
 
 run_game()
 
